e9ed8123077a488d922c7d1a8c5d823a23f30153
Rohit Verma <rohitgcode20@gmail.com>
2026-05-24 10:53:10 +0530

    Fix import path case sensitivity for CartItem

--- src/Pages/Cart.jsx
+++ src/Pages/Cart.jsx
@@ -1,5 +1,5 @@
 import { useEffect } from "react";
-import CartItem from "../components/CartItem";
+import CartItem from "../Components/CartItem";
 
 const Cart = ({ cart, removeFromCart }) => {
   useEffect(() => {
